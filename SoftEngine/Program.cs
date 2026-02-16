57ebf0046fdaf368f40df2e47e4a1013f52e46cd
8fclss <8fclss@gmail.com>
2026-02-16 17:07:21 +0300

    MVP

--- SoftEngine/Program.cs
+++ SoftEngine/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SoftEngine;
 
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main()
+    {
+        // Setup
+        int width = Console.WindowWidth;
+        int height = Console.WindowHeight;
+
+        Vector3 cubePoint = new Vector3(5, -2, 10);
+
+        // Game loop
+        while (true)
+        {
+            // Logic
+            if (cubePoint.Z > 1)
+            {
+                cubePoint.Z -= 0.1f;
+            }
+            else
+            {
+                cubePoint.Z = 20;
+            }
+            
+            // Rendering
+            Console.Clear();
+
+            // Projection formula
+            float fov = height / 2.0f;
+            
+            // Calculate raw 2D position
+            int screenX = (int)(cubePoint.X / cubePoint.Z * fov);
+            int screenY = (int)(cubePoint.Y / cubePoint.Z * fov);
+            
+            // Centering on screen
+            screenX += width / 2;
+            screenY += height / 2;
+            
+            // Bounds check 
+            if (screenX >= 0 && screenX < width && screenY >= 0 && screenY < height)
+            {
+                Console.SetCursorPosition(screenX, screenY);
+                Console.Write("0");
+            }
+            
+            // Timing
+            Thread.Sleep(33);
+        }
+    }
+}
---
+++ SoftEngine/Vector3.cs
@@ -0,0 +1,16 @@
+namespace SoftEngine;
+
+
+public struct Vector3
+{
+    public float X;
+    public float Y;
+    public float Z;
+
+    public Vector3(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
