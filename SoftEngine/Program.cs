a35c31121378c043728913be4bcb52dee3e5ecf0
8fclss <8fclss@gmail.com>
2026-02-24 08:15:06 +0300

    feat: bresenham's algorithm, square

--- SoftEngine/Program.cs
+++ SoftEngine/Program.cs
@@ -13,10 +13,10 @@ class Program
 
         var points = new List<Vector3>
         {
-            new Vector3(-2,  2, 10),
-            new Vector3( 2,  2, 10),
-            new Vector3( 2, -2, 10),
-            new Vector3(-2, -2, 10)
+            new Vector3(-2,  2, 20),
+            new Vector3( 2,  2, 20),
+            new Vector3( 2, -2, 20),
+            new Vector3(-2, -2, 20)
         };
 
         while (true)
@@ -38,20 +38,26 @@ class Program
             Array.Fill(buffer, ' ');
 
             float fov = height;
+            var projectedPoints = new (int x, int y)[points.Count];
 
-            foreach (var point in points)
+            for (int i = 0; i < points.Count; i++)
             {
+                var point = points[i];
                 float projectedX = (point.X / point.Z) * fov;
                 float projectedY = (point.Y / point.Z) * fov;
 
-                int x = (int)(projectedX + (width / 2.0f));
-                int y = (int)(projectedY + (height / 2.0f));
+                projectedPoints[i] = (
+                    (int)(projectedX + (width / 2.0f)),
+                    (int)(projectedY + (height / 2.0f))
+                );
+            }
 
-                if (x >= 0 && x < width && y >= 0 && y < height - 1)
-                {
-                    int index = y * width + x;
-                    buffer[index] = 'O';
-                }
+            for (int i = 0; i < projectedPoints.Length; i++)
+            {
+                var p1 = projectedPoints[i];
+                var p2 = projectedPoints[(i + 1) % projectedPoints.Length];
+
+                DrawLine(p1.x, p1.y, p2.x, p2.y, buffer, width, height);
             }
 
             if (Console.WindowWidth == width && Console.WindowHeight == height)
@@ -68,6 +74,36 @@ class Program
             }
 
             Thread.Sleep(33);
+        }
+    }
+
+    static void DrawLine(int x0, int y0, int x1, int y1, char[] buffer, int width, int height)
+    {
+        int dx = Math.Abs(x1 - x0);
+        int dy = -Math.Abs(y1 - y0);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx + dy;
+
+        while (true)
+        {
+            if (x0 >= 0 && x0 < width && y0 >= 0 && y0 < height - 1)
+            {
+                buffer[y0 * width + x0] = '#';
+            }
+
+            if (x0 == x1 && y0 == y1) break;
+            int e2 = 2 * err;
+            if (e2 >= dy)
+            {
+                err += dy;
+                x0 += sx;
+            }
+            if (e2 <= dx)
+            {
+                err += dx;
+                y0 += sy;
+            }
         }
     }
 }
